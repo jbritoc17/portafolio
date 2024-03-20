b76b49845c2769e9f0c8b30f75d1e82ad1ad257a
jbritoc17 <jbritoc17@outlook.es>
2024-03-20 11:51:02 -0500

    creando index

---
+++ Proyectos Desarrollo - Front end/Portafolio/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const path = require("path")
+
+let app = express();
+app.use(express.static("public"));
+
+app.listen(3000,()=>{
+    console.log("se escucha en http://localhost:3000/");
+})
+
+app.get("/",(req,res)=>{
+    let index = path.join(__dirname,"./views/index.html")
+    res.sendFile(index)
+})
